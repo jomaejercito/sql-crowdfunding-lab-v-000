4067fef33aee65ed84057d600f774f557e9fe2f8
Joma Ejercito <jomaejercito@gmail.com>
2018-03-03 21:41:43 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -2,9 +2,9 @@ CREATE TABLE projects(
   id INTEGER PRIMARY KEY,
   title TEXT,
   category TEXT,
-  funding goal INTEGER,
+  funding_goal INTEGER,
   start_date TEXT,
-  end_date TEXT,
+  end_date TEXT
 );
 
 CREATE TABLE users(
